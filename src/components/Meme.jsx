cb35de3aa10dc3aee6447f59e2264d4a37d8529b
Natuza <natuza.machado@gmail.com>
2023-02-22 17:12:34 -0500

    added upper and lower text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,20 +3,52 @@ import '../index.css'
 import memesData from "../../memesData"
 
 export default function Meme(){
-    const [memeImage, setMemeImage] = useState("")
-
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    const [meme, setMeme] = useState(
+        {
+          topText:"",
+          bottomText:"",
+          randomImage: "http://i.imgflip.com/1bij.jpg"
+        }
+    )
+    function handleChange(event){
+        const { name, value } = event.target
+        setMeme((oldMeme) => ({
+            ...oldMeme,
+            [name]: value
+        }))
+    }
     function getMemeImage() {
-        let memeIndex = Math.floor(Math.random() * memesData.data.memes.length)
+        const memesArray = allMemeImages.data.memes
+        let memeIndex = Math.floor(Math.random() * memesArray.length)
         let newMemeImg = memesData.data.memes[memeIndex].url
-        setMemeImage(newMemeImg)
+        setMeme((prevMeme)=>({
+            ...prevMeme,
+            randomImage: newMemeImg
+        }))
     }
     
     return (
         <div className="box">
             <section>
+                
                <div className="input">
-                    <input type= "text" placeholder="Top text"></input>
-                    <input type = "text" placeholder="Bottom text"></input>
+                    <input 
+                        type="text"
+                        placeholder="top text"
+                        className="form--input"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                    <input 
+                        type = "text"
+                        placeholder="Bottom text"
+                        className="form--input"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
                </div>
                <div className="btn">
                     <button onClick = {getMemeImage} >
@@ -25,7 +57,9 @@ export default function Meme(){
                </div>  
             </section>
             <div className="img">
-                <img src={memeImage} />
+                <img src={meme.randomImage} className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div> 
         </div>
     )
